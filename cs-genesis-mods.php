fdf017a50c65a2929951ff6d5919a7cdb1823b31
Bradford Knowlton <brad@ucanbonline.net>
2014-11-03 14:45:40 -0700

    Right aligned logo in header

--- cs-genesis-mods.php
+++ cs-genesis-mods.php
@@ -3,7 +3,7 @@
 Plugin Name: CS Genesis Mods
 Plugin URI: https://github.com/ClearSkyPDX/CS-genesis-mods/
 Description: Add additional customizations to Genesis Theme.
-Version: 1.8.6
+Version: 1.9.1
 Author: Bradford Knowlton
 Author URI: http://bradknowlton.com/
 License: GNU General Public License v2
@@ -16,16 +16,27 @@ GitHub Branch: clearskypdx.com
 
 function CS_after_setup_theme() {
 	remove_action( 'genesis_header', 'genesis_do_header' );
-	add_action( 'genesis_header', 'genesis_do_new_header' );
+	// add_action( 'genesis_header', 'genesis_do_new_header' );
+	remove_action( 'genesis_after_header', 'beautiful_site_header_banner' );
+	add_action( 'genesis_after_header', 'clearsky_site_header_banner' );
 	add_action('wp_head','hook_css');
 }
 add_action( 'after_setup_theme', 'CS_after_setup_theme' );
 
-function genesis_do_new_header() {
-	
+function clearsky_site_header_banner() {
+
+	if ( ! get_background_image() )
+		return;
+	add_action( 'genesis_header', 'genesis_do_header' );
+	echo '<div class="site-header-banner">';
+	do_action( 'genesis_header');
+	echo '</div>';
+
 }
+
+
 function hook_css()
 {
-	$output="<style></style>";
+	$output="<style>.header-full-width.header-image .site-title a {background-position: right center !important;}</style>";
 	echo $output;
 }
